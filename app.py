ed0bdcdb51deb19de37269b5dcfd718486009a52
1111-2002 <107058155+1111-2002@users.noreply.github.com>
2022-07-16 18:16:04 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st 
+import joblib 
+
+#load the joblib model 
+model_nb = joblib.load('Cars.csv')
+
+#user input 
+st.title("Cars body-style")
+ip = st.text_input("Enter your message:")
+
+#predict if the entered message belongs to sports,world,politics 
+op = model_nb.predict([ip])
+if st.button('Predict Category'):
+  st.title(op[0])  #prints the output as  sports or politics or world
+           
+
+  
+                                    
+  
