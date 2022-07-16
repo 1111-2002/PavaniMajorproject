b12aeb5095102393b93267e7b99a7a9206371f57
1111-2002 <107058155+1111-2002@users.noreply.github.com>
2022-07-16 18:29:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ ip = st.text_input("Enter your message:")
 
 #predict if the entered message belongs to sports,world,politics 
 op = model_nb.predict([ip])
-if st.button('Predict Category'):
+if st.button('Predict style'):
   st.title(op[0])  #prints the output as  sports or politics or world
              
            
