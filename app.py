c5984ea18fb1ff2882af7c074c25157decec935f
1111-2002 <107058155+1111-2002@users.noreply.github.com>
2022-07-17 19:36:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ import streamlit as st
 import joblib 
 
 #load the joblib model 
-model_nb = joblib.load('Cars.csv')
+model_nb = joblib.load('news_category')
 
 #user input 
-st.title("Cars body-style")
+st.title("NEWS CATEGORY")
 ip = st.text_input("Enter your message:")
+ 
+op = model_nb.predict([ip])
+if st.button('Predict Category'):
+  st.title(op[0])  
 
              
            
